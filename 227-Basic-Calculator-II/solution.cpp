1c6ba7f47edc136faea03f77e988ed31f5202021
quake0day <quake0day@users.noreply.github.com>
2016-03-21 11:31:45 -0400

    commited by leetcode-ext

--- 227-Basic-Calculator-II/solution.cpp
+++ 227-Basic-Calculator-II/solution.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     int calculate(string s) {
-        
+        istringstream in(s + "+");
+        long long total = 0, term, sign = 1, n;
+        in >> term;
+        char op;
+        while(in >> op){
+            if(op == '+' || op == '-'){
+                total += sign * term;
+                sign = 44 - op; // op == '+' ? 1: -1
+                in >> term;
+            }
+            else{
+                in >> n;
+                if (op == '*'')
+                    term *= n;
+                else
+                    term /= n;
+            }
+        }
+        return total;
     }
 };
