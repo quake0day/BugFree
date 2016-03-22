36467d60288670e31dec1f1cd6d24d82f314fc5a
quake0day <quake0day@users.noreply.github.com>
2016-03-22 18:10:30 -0400

    [Wrong Answer]committed by LeetCode Extension

---
+++ 165-Compare-Version-Numbers/solution.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def compareVersion(self, version1, version2):
+        """
+        :type version1: str
+        :type version2: str
+        :rtype: int
+        """
+        if not version1 and not version2:
+            return 0
+        elif not version1:
+            return -1
+        elif not version2:
+            return 1
+        l1 = version1.split(".")[::-1]
+        l2 = version2.split(".")[::-1]
+        for i in xrange(min(len(l1), len(l2))):
+            if int(l1[i]) > int(l2[i]):
+                return 1
+            elif int(l1[i]) < int(l2[i]):
+                return -1
+        return 0
+        
