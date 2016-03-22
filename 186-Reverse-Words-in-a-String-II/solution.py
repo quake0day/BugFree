3e2257e1f30889090d242570a99df8db398eddc5
quake0day <quake0day@users.noreply.github.com>
2016-03-22 17:57:04 -0400

    [Runtime Error]committed by LeetCode Extension

---
+++ 186-Reverse-Words-in-a-String-II/solution.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: a list of 1 length strings (List[str])
+        :rtype: nothing
+        """
+        n = len(s)
+        l, r = 0, 0
+        while r < n:
+            if s[r] != ' ':
+                r += 1
+            elif s[r] == ' ':
+                self.reverse(l, r-1, s)
+                r += 1
+                l = r
+
+        return self.reverse(0, n, s)
+            
+    
+    def reverse(self, l, r, s):
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
