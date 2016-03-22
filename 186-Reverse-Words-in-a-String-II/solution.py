388374e1f0b2a3076f0fea064af88e8a83e2abb9
quake0day <quake0day@users.noreply.github.com>
2016-03-22 18:04:54 -0400

    [Accepted]committed by LeetCode Extension

--- 186-Reverse-Words-in-a-String-II/solution.py
+++ 186-Reverse-Words-in-a-String-II/solution.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type s: a list of 1 length strings (List[str])
         :rtype: nothing
         """
-        self.reverse(s, 0, len(s) - 1)
+        self.reverse( 0, len(s) - 1, s)
         n = len(s)
         beg = 0
         for r in xrange(len(s)):
