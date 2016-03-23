6e10445fae3c6cf6c6aff4ae8562f6afa694ffee
quake0day <quake0day@users.noreply.github.com>
2016-03-22 22:58:25 -0400

    [Runtime Error]committed by LeetCode Extension

---
+++ 58-Length-of-Last-Word/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = s.split()
+        k = len(s)-1
+        while not s[k] and k >= 0:
+            k -= 1
+        return len(s[k]) if k >= 0 else 0
+        
