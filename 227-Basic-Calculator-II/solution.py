725dcc949ffdb733afe870a65153a4ea39cb5369
quake0day <quake0day@users.noreply.github.com>
2016-03-23 16:30:38 -0400

    [Accepted]committed by LeetCode Extension

--- 227-Basic-Calculator-II/solution.py
+++ 227-Basic-Calculator-II/solution.py
@@ -4,4 +4,26 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        def nextint(s,i):
+            res = 0
+            while i < len(s) and s[i] not in '+-*/':
+                res = res * 10 + ord(s[i]) - ord('0')
+                i += 1
+            return (res, i)
         
+        s = s.replace(' ', '')
+        stack = []
+        i = 0
+        tmp, i = nextint(s,0)
+        stack.append(tmp)
+        while i < len(s):
+            op = s[i]
+            tmp, i = nextint(s, i+1)
+            if op == '+': stack.append(tmp)
+            elif op == '-': stack.append(-tmp)
+            elif op == '*': stack[-1] *= tmp
+            elif op == '/': stack[-1] = stack[-1] // tmp if stack[-1] >= 0 else -((-stack[-1])//tmp)
+        res = 0
+        for num in stack:
+            res += num
+        return res
