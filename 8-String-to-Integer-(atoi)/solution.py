73b1a2ba1f0812dd8307ca7bddc5c212d5b03241
quake0day <quake0day@users.noreply.github.com>
2016-03-22 21:10:43 -0400

    [Runtime Error]committed by LeetCode Extension

---
+++ 8-String-to-Integer-(atoi)/solution.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def myAtoi(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+        INT_MAX = 2147483647
+        INT_MIN = -2147483648
+        s = s.lstrip()
+        sign = 0
+        if s[0] in ('+','-'):
+            sign = 1 if s[0] == '-'
+            s = s[1:]
+        res = 0
+        digits = map(str, [i for i in xrange(10)])
+        i = 0
+        while i < len(s) and s[i] in digits:
+            res = res * 10 + int(s[i])
+            i += 1
+        if res > INT_MAX:
+            return INT_MAX
+        if res < INT_MIN:
+            return INT_MIN
+        return res * sign
+        
