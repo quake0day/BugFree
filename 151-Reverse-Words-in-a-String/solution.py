a2e9d498f51d47005975f1c91371a78d5ca70e60
quake0day <quake0day@users.noreply.github.com>
2016-03-22 22:54:36 -0400

    [Wrong Answer]committed by LeetCode Extension

--- 151-Reverse-Words-in-a-String/solution.py
+++ 151-Reverse-Words-in-a-String/solution.py
@@ -4,19 +4,5 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        s = s.lstrip()
-        q = collections.deque()
-        l,r = 0, 0
-        res = []
-        while r < len(s):
-            if r != " ":
-                r += 1
-            elif r == " ":
-                q.append(s[l:r-1])
-                r += 1
-                l = r
-        q.append(s[l:r-1])
-        while q:
-            res.append(q.pop())
-        return "".join(res)
+        return s.split()[::-1]
                 
