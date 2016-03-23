bc303988198e4b8ed171dfc1448d6c8179a500bc
quake0day <quake0day@users.noreply.github.com>
2016-03-22 21:36:52 -0400

    [Wrong Answer]committed by LeetCode Extension

---
+++ 53-Maximum-Subarray/solution.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        n = len(nums)
+        l, r = 0, 0
+        maxSum = -float('inf')
+        curSum = 0
+        while r < n:
+            curSum += nums[r]
+            if curSum >= 0:
+                maxSum = max(curSum, maxSum)
+                r += 1
+            else:
+                r += 1
+                l = r
+                
+        return maxSum
