63e9018661aac77b7850cc77315534f4e706c1e3
quake0day <quake0day@users.noreply.github.com>
2016-03-29 16:24:16 -0400

    using bit to generate powerset

---
+++ 78-Subsets/solution_bit.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        n = len(nums)
+        size = 2 ** n
+        res = []
+        for c in xrange(size):
+            tmp = []
+            for j in xrange(n):
+                if c & (1 << j):
+                    tmp.append(nums[j])
+            res.append(tmp)
+        return res
